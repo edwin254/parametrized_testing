6ceeaa6e4a266cab2eafb1119066025c77571c66
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-09-01 13:47:36 +0300

    [Chore]Code refactor

--- test_parametrization.py
+++ test_parametrization.py
@@ -1,10 +1,11 @@
 import pytest
+from numberWork import runOddEvenCheck
 
 
 @pytest.mark.parametrize("test_input,expected", [
-    ("3+5", 8),
-    ("2*4", 8),
-    ("6*9", 42),
+    (3, "odd"),
+    (4, "even"),
+    (9, "even"),
 ])
-def test_eval(test_input, expected):
-    assert eval(test_input) == expected
+def test_even_and_odd_numbers(test_input, expected):
+    assert (runOddEvenCheck(test_input)) == expected
